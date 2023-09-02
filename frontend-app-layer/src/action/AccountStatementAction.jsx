e41fa2cdb5188432d1e849c89c39b7e03038ad92
prashantapal <ext-prashanta.pal@elisa.fi>
2023-09-02 05:46:44 +0300

    Fixed table view

--- frontend-app-layer/src/action/AccountStatementAction.jsx
+++ frontend-app-layer/src/action/AccountStatementAction.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import api from "../api/AccountStatementApi"
+import { Oval } from 'react-loader-spinner';
+import api from '../api/AccountStatementApi';
+import AccountStatementView from "../view/AccountStatementView";
 
 const AccountStatementAction = () => {
   const [isLoading, setLoading] = useState(true);
-  const [isLoaded, setLoaded] = useState(false);
+  const [isAuthError, setAuthError] = useState(false);
   const [isError, setError] = useState(false);
   const [accountStatements, setAccountStatements] = useState([]);
 
@@ -12,11 +14,50 @@ const AccountStatementAction = () => {
       console.log(response.data)
       setAccountStatements(response.data)
       setLoading(false)
-      setLoaded(true)
     }).catch(error => {
       console.log(error)
-      setError(true)
+      if (error.response.status == 401) {
+        setAuthError(true)
+      } else {
+        setError(true)
+      }
     })
   }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  } else if (isAuthError) {
+    return <AuthError />;
+  } else if (isError) {
+    return <Error />
+  } else {
+    return <AccountStatementView {...{accountStatements}} />
+  }
 }
+
+const Loading = () => (
+  <div className="App">
+    <Oval
+      ariaLabel="loading-indicator"
+      height={80}
+      width={80}
+      strokeWidth={3}
+      strokeWidthSecondary={3}
+      color="blue"
+      secondaryColor="lightblue"
+    />
+  </div>
+);
+
+const AuthError = () => (
+  <div className="App">
+    <h3>Invalid username or password!!!</h3>
+  </div>
+);
+
+const Error = () => (
+  <div className="App">
+    <h3>Internal server error!!!</h3>
+  </div>
+);
 export default AccountStatementAction;
--- frontend-app-layer/src/view/AccountStatementView.jsx
+++ frontend-app-layer/src/view/AccountStatementView.jsx
@@ -1,29 +1,85 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 import moment from "moment";
 
-const AccountStatementView = (accountStatements) => {
-
-}
-
-const AccountTransactions = (accountTransactions) => (
+const AccountStatementView = ({ accountStatements }) => (
   <>
-    <table>
-      <tr>
-        {accountTransactions.map(accountTransaction => <Transaction transaction = {accountTransaction} />)}
-      </tr>
-    </table>
+    {accountStatements.map(accountStatement =>
+      <Table>
+        <tbody>
+        <tr>
+          <td>{monthlyTransaction(accountStatement)}</td>
+          <td><MonthlyClosingBalance {...{accountStatement}} /></td>
+        </tr>
+        </tbody>
+      </Table>
+    )}
   </>
 )
 
-const Transaction = (transaction) => (
+const monthlyTransaction = (accountStatement, showTableHeader = false) => (
   <>
-    <td>{moment(transaction.transactionDate).format("DD.MM.YYYY HH:mm:ss")}</td>
-    <td>{transaction.transactionId}</td>
-    <td>{transaction.transactionDescription}</td>
-    {isCreditType(transaction) ?
-      <td>(+){transaction.transactionAmount}</td>
-      : <td>(-){transaction.transactionAmount}</td>}
+    <Table>
+      {showTableHeader &&
+        <thead>
+          <tr>
+            <th>Transaction</th>
+            <th>Closing balance(cumulative)</th>
+          </tr>
+        </thead>
+      }
+        <tbody>
+          <tr>
+            <td>{accountTransactions(accountStatement.transactions)}</td>
+            <td></td>
+          </tr>
+        </tbody>
+    </Table>
+  </>
+)
+
+const MonthlyClosingBalance = ({ accountStatement }) => (
+  <>
+    <tr>
+      <td>{moment(accountStatement.month).format("MMMM, YYYY")}  Total credit: {accountStatement.totalCredit.toFixed(2)}   Total debit: {accountStatement.totalDebit.toFixed(2)}   Closing balance: {accountStatement.balance.toFixed(2)}</td>
+      <td>{accountStatement.cumulativeBalance.toFixed(2)}</td>
+    </tr>
+  </>
+)
+
+const accountTransactions = (accountTransactions, showTableHeader = false) => (
+  <>
+    <Table>
+      {showTableHeader &&
+        <thead>
+          <tr>
+            <th>Transaction Date</th>
+            <th>Transaction Id</th>
+            <th>Description</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+      }
+      <tbody>
+        {accountTransactions.map(transaction => <Transaction  {...{transaction}} />)}
+      </tbody>
+    </Table>
+  </>
+)
+
+const Transaction = ({ transaction }) => (
+  <>
+    <tr>
+      <td>{moment(transaction.transactionDate).format("DD.MM.YYYY HH:mm:ss")}</td>
+      <td>{transaction.transactionId}</td>
+      <td>{transaction.transactionDescription}</td>
+      {isCreditType(transaction) ?
+        <td>(+){transaction.transactionAmount.toFixed(2)}</td>
+        : <td>(-){transaction.transactionAmount.toFixed(2)}</td>}
+    </tr>
   </>
 )
 
 const isCreditType = (accountTransaction) => accountTransaction.transactionType === "CREDIT";
+
+export default AccountStatementView;
