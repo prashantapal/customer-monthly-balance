881ba4eb0f0c80e2e3cac0ec1492bb1f1374cbc4
prashantapal <ext-prashanta.pal@elisa.fi>
2023-09-03 15:47:42 +0300

    Fixed table view

--- frontend-app-layer/src/App.js
+++ frontend-app-layer/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import './style/AccountStatement.css'
+import React, { useState } from "react"
+import ReactDOM from "react-dom";
 import AccountStatementAction from "./action/AccountStatementAction"
+import logo from "./image/logo.jpg"
+import "./style/accountStatement.css"
 
 function App() {
-  return <AccountStatementAction />
+
+  return(
+    <>
+      <div className="header">
+        <img src={logo} alt="HCDL Bank Logo" />
+      </div>
+    <AccountStatementAction />
+    </>
+  )
 }
 
 export default App;
